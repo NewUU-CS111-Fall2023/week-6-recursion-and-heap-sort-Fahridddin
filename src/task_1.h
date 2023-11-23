844ac7dfa14b284fa0243191752e7a354803b667
Name Surname <20221500282@newuzbekistanuniversity.uz>
2023-11-23 23:56:20 +0500

    1st attemp

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,18 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+
+  if (a < b) {
+    for (int i = a; i <= b; ++i) {
+      std::cout << i << " ";
+    }
+  } else {
+    for (int i = a; i >= b; --i) {
+      std::cout << i << " ";
+    }
+  }
+
+  return 0;
+}
